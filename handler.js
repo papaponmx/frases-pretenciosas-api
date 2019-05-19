2e812c3c25c261602965877da9c14626e4eb254d
Jaime Rios <jaimemadrigalrios@gmail.com>
2019-05-18 23:34:07 -0500

    Updated project to use Dynamo.

--- handler.js
+++ handler.js
@@ -1,14 +1,62 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const databaseManager = require('./databaseManager');
+const uuidv1 = require('uuid/v1');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+function createResponse(statusCode, message) {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(message)
+  };
+}
+
+module.exports.saveItem = (event, context, callback) => {
+  const item = JSON.parse(event.body);
+  console.log(item);
+  item.itemId = uuidv1();
+
+  databaseManager.saveItem(item).then(response => {
+    console.log(response);
+    callback(null, createResponse(200, response));
+  });
+};
+
+module.exports.getItem = (event, context, callback) => {
+  const itemId = event.pathParameters.itemId;
+
+  databaseManager.getItem(itemId).then(response => {
+    console.log(response);
+    callback(null, createResponse(200, response));
+  });
+};
+
+module.exports.deleteItem = (event, context, callback) => {
+  const itemId = event.pathParameters.itemId;
+
+  databaseManager.deleteItem(itemId).then(response => {
+    callback(null, createResponse(200, 'Item was deleted'));
+  });
+};
+
+module.exports.updateItem = (event, context, callback) => {
+  const itemId = event.pathParameters.itemId;
+
+  const body = JSON.parse(event.body);
+  const paramName = body.paramName;
+  const paramValue = body.paramValue;
+
+  databaseManager.updateItem(itemId, paramName, paramValue).then(response => {
+    console.log(response);
+    callback(null, createResponse(200, response));
+  });
+};
+
+module.exports.triggerStream = (event, context, callback) => {
+  console.log('trigger stream was called');
+
+  const eventData = event.Records[0];
+  //console.log(eventData);
+
+  console.log(eventData.dynamodb.NewImage);
+  callback(null, null);
 };
